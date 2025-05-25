e6670f8de3a90d5c647858a13aa5dd2a6b4e3280
Abhilash695 <abilashgowda093@gmail.com>
2025-05-25 14:26:10 +0530

    uodated code in c branch

--- file1.py
+++ file1.py
@@ -10,3 +10,43 @@ from itertools import islice
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 
+
+
+# ai_features.py
+from textblob import TextBlob
+from gensim.summarization import keywords, summarize
+import spacy
+from collections import Counter
+import os
+
+# Load spaCy model safely
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    os.system("python -m spacy download en_core_web_sm")
+    nlp = spacy.load("en_core_web_sm")
+
+class AIAnalyzer:
+    def _init_(self, text):
+        self.text = text
+        self.blob = TextBlob(text)
+        self.doc = nlp(text)
+
+    def get_sentiment(self):
+        polarity = self.blob.sentiment.polarity
+        subjectivity = self.blob.sentiment.subjectivity
+        return {"polarity": polarity, "subjectivity": subjectivity}
+
+    def get_sentence_level_sentiment(self):
+        return [{"sentence": str(sent), "sentiment": TextBlob(str(sent)).sentiment.polarity}
+                for sent in self.blob.sentences]
+
+    def get_keywords(self):
+        try:
+            return keywords(self.text, ratio=0.1, words=10).split('\n')
+        except:
+            return ["Not enough data to extract keywords."]
+
+
+
+
